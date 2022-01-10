bfb9920272d41c346c9ab78d2e433ba7c5295492
nbagd@outlook.com <you@example.com>
2022-01-10 13:48:33 +0530

    second project in git

---
+++ src/main/java/Gitpracticeone.java
@@ -0,0 +1,6 @@
+
+public class Gitpracticeone {
+public static void main(String[] args) {
+	System.out.println("git practice one");
+}
+}
---
+++ src/main/java/Gitpracticetwo.java
@@ -0,0 +1,6 @@
+
+public class Gitpracticetwo {
+public static void main(String[] args) {
+	System.out.println("git practice two");
+}
+}
